dccaa2bc58519f544ae9d99318fa2c2ea0fc65ef
Oeromilk <oero.mccomb@gmail.com>
2016-09-29 15:57:09 -0400

    question 1 completed

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+(function(){
+  "use strict";
 
+// The function to return the total price of the items.
+var itemTotal = items.reduce(function(total, item){
+  console.log(item.price);
+  return total + item.price;
+}, 0);
+
+// The log that will tell us the average price of items divided by the amount
+// of items in the array.
+console.log('The average price is: ' + (itemTotal/items.length).toFixed(2) );
+
+
+
+})();
